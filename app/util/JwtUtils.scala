1d4778bd629e937807f1c1fa7e0125e713fb1930
ayxan386 <ayxanpro@gmail.com>
2021-01-01 11:35:00 +0400

    Moven Jwt into new class

---
+++ app/util/JwtUtils.scala
@@ -0,0 +1,24 @@
+package util
+
+import io.jsonwebtoken.{Jwts, SignatureAlgorithm}
+
+import java.time.Instant
+import java.time.temporal.ChronoUnit
+import java.util.Date
+import javax.inject.Singleton
+
+@Singleton
+class JwtUtils {
+
+  val secret = "hello_world"
+
+  def buildToken(nickname: String): String = Jwts
+    .builder()
+    .setSubject(nickname)
+    .setExpiration(getExpiration)
+    .signWith(SignatureAlgorithm.HS256, secret.getBytes())
+    .compact()
+
+
+  def getExpiration = new Date(Instant.now().plus(3, ChronoUnit.HOURS).getNano)
+}
